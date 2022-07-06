0c91f98ff273fe34b6ba244e19e96a733dec2c07
ailsonazevedo <61508532+ailsonazevedo@users.noreply.github.com>
2022-07-06 08:26:14 -0300

    feat: add category model

--- products/admin.py
+++ products/admin.py
@@ -1,18 +1,32 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products, Category
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price', 'image')
-    list_filter = ('name', 'description', 'price', 'image')
+    list_display = ('name', 'description', 'price', 'image', 'category')
+    list_filter = ('name', 'description', 'price', 'image','category')
     search_fields = ('name', 'description', 'price', 'image')
     ordering = ('name', 'description', 'price', 'image')
     fieldsets = (
         ('Product', {
-            'fields': ('name', 'description', 'price', 'image')
+            'fields': ('name', 'description', 'price', 'image','category')
         }),
     )
     readonly_fields = ('created_at', 'updated_at')
     class Meta:
         model = Products
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+    fieldsets = (
+        ('Category', {
+            'fields': ('name',)
+        }),
+    )
+    class Meta:
+        model = Category
 # Register your models here.
--- products/models.py
+++ products/models.py
@@ -1,17 +1,27 @@
 from django.db import models
 
 
-
 class BaseModel(models.Model):
     created_at = models.DateTimeField('Criação:',auto_now_add=True)
     updated_at = models.DateTimeField('Atualização:',auto_now=True)
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        managed = True
+        verbose_name = ('Category')
+        verbose_name_plural = ('Categories')
+
 
 class Products(BaseModel):
-    name = models.CharField('Nome:',max_length=100)
+    name = models.CharField('Nome:', max_length=150)
     description = models.TextField('Descrição:')
-    price = models.DecimalField('Preço:',max_digits=10, decimal_places=2)
-    image = models.ImageField('Imagem:',upload_to='media/products', null=True, blank=True)
+    price = models.DecimalField('Preço:', max_digits=10, decimal_places=2)
+    image = models.ImageField('Imagem:', upload_to='media/products', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name='categories')
     
     def __str__(self):
         return self.name
@@ -20,6 +30,5 @@ class Products(BaseModel):
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
         ordering = ('name',)
-
 
 # Create your models here.
