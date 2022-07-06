e5e41934828a06037bc3d0e73bf618b9e5d273eb
ailsonazevedo <61508532+ailsonazevedo@users.noreply.github.com>
2022-07-05 21:42:58 -0300

    feat: add products model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Products
 
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'image')
+    list_filter = ('name', 'description', 'price', 'image')
+    search_fields = ('name', 'description', 'price', 'image')
+    ordering = ('name', 'description', 'price', 'image')
+    fieldsets = (
+        ('Product', {
+            'fields': ('name', 'description', 'price', 'image')
+        }),
+    )
+    readonly_fields = ('created_at', 'updated_at')
+    class Meta:
+        model = Products
 # Register your models here.
--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField('Criação:',auto_now_add=True)
+    updated_at = models.DateTimeField('Atualização:',auto_now=True)
+
+
+class Products(BaseModel):
+    name = models.CharField('Nome:',max_length=100)
+    description = models.TextField('Descrição:')
+    price = models.DecimalField('Preço:',max_digits=10, decimal_places=2)
+    image = models.ImageField('Imagem:',upload_to='media/products', null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        ordering = ('name',)
+
+
 # Create your models here.
