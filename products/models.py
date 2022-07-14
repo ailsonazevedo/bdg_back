8a5932e00011422597a3cdb5edbf6e60b870f9b3
ailsonazevedo <61508532+ailsonazevedo@users.noreply.github.com>
2022-07-14 10:27:45 -0300

    feat: add slug category and product

--- products/models.py
+++ products/models.py
@@ -11,9 +11,13 @@ class BaseModel(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(default='')
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return f"/{self.slug}"
     class Meta:
         managed = True
         verbose_name = ('Category')
@@ -26,14 +30,34 @@ class Products(BaseModel):
     price = models.DecimalField('Preço:', max_digits=10, decimal_places=2)
     image = models.ImageField('Imagem:', upload_to=upload_image, null=True, blank=True)
     categoria = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name='categories')
-    # slug = models.SlugField(null=False, unique=True)
+    slug = models.SlugField(default='')
     
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return f"/{self.categoria.slug}/{self.slug}"
 
     class Meta:
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
         ordering = ('name',)
 
+class Ordered(BaseModel):
+    product = models.ManyToManyField(Products, related_name='items')
+    quantity = models.IntegerField('Quantidade:', default=1)        
+    price = models.DecimalField('Preço:', max_digits=9, decimal_places=2)
+    total = models.DecimalField('Preço Total:', max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.id
+
+    # def total(self):
+    #     return self.product.price * self.quantity
+
+    class Meta:
+        verbose_name = 'Ordered'
+        verbose_name_plural = 'Ordered'
+        ordering = ('-created_at',)
+        
 # Create your models here.
