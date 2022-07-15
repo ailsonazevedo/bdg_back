7f775c922518b0b57bf458042a668565929904cc
ailsonazevedo <61508532+ailsonazevedo@users.noreply.github.com>
2022-07-15 08:40:28 -0300

    feat: add swagger api

--- bdg_back/urls.py
+++ bdg_back/urls.py
@@ -17,18 +17,37 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.db import router
-from django.urls import include, path
+from django.urls import include, path, re_path
 from products import views
 import products
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="BDG API",
+      default_version='v1',
+      description="Api para o sistema de vendas do BDG",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=False,
+   permission_classes=[permissions.AllowAny],
+)
 
 # route = router.DefaultRouter()
 # route.register(r'produtos', ProductViewsets, basename = 'Products')
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('/products', include(route.urls),name='produtos')
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 if settings.DEBUG:
